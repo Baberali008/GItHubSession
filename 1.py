0fb2ad9c50325739db561e0d175267493271f89b
Baber Ali <babarali.ch90@gmail.com>
2022-10-22 13:52:36 +0500

    my first commit

---
+++ 1.py
@@ -0,0 +1,18 @@
+#print(2+4)
+#print("Alham dulliah")
+#print("Insha alllah i will become a data scientist")
+#print(10/3)
+#print(10//3)
+print(2**4)
+# #print(2+3-8/2+(2**4)+12)
+# a = "Baber"
+# print(type(a))
+# y = "we are learning python with ammar"
+# print(y)
+# fruit_basket = "Mange"
+# print(fruit_basket)
+
+
+# A = input("which is your best fruit? ")
+
+name = i
